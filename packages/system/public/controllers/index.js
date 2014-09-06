003506d1e42463570e6926baf8edfe823ec9310d
Pablo Karlsson <pablo.karlsson@gmail.com>
2014-09-06 15:59:44 +0200

    modal

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 angular.module('mean.system').controller('IndexController', ['$scope', '$modal', 'Global',
-  function($scope, $modal, Global) {
+  function($scope, $modal, $log, Global) {
     $scope.global = Global;
 
     var mapOptions = {
@@ -39,5 +39,49 @@ angular.module('mean.system').controller('IndexController', ['$scope', '$modal',
     $scope.toggleMenu = function(element) {
       element.toggled = !element.toggled;
     };    
+    
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function (size) {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'myModalContent.html',
+        controller: ModalInstanceCtrl,
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+    
+    //
+    
+    var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+      $scope.items = items;
+      $scope.selected = {
+        item: $scope.items[0]
+      };
+
+      $scope.ok = function () {
+        $modalInstance.close($scope.selected.item);
+      };
+
+      $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+      };
+    };
+    
+    $scope.open();
+    //
   }
 ]);
+
